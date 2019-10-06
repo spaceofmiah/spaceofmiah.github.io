5a69eaa9e687c04d5007cc35044b97d13cb3d849
spaceofmiah <geek_200@outlook.com>
2019-10-06 21:48:03 +0100

    Add a reponsive navbar

--- assets/scripts/index.js
+++ assets/scripts/index.js
@@ -1,24 +1,19 @@
-// toggles navbar
+/**
+ * toggling navbar
+ */
+const navbar_toggler = document.getElementById('nav_toggler');
+const navbar_links = document.getElementById('navbar_links');
+navbar_toggler.addEventListener('click', () => {
+    navbar_links.classList.toggle('ani-smooth-out');
+    navbar_links.classList.toggle('active_navbar');
+});
 
-const navbar_toggler = document.getElementById('navbar-toggler');
-const navbar_content = document.getElementById('navbar-content');
-const content = document.getElementById('content');
-
-let flag = false;
-navbar_toggler.addEventListener('click', ($event) => {
-    if (flag === false){
-        navbar_toggler.classList.remove('smooth-right-move');
-        navbar_toggler.classList.add('smooth-left-move');
-        flag = true
-        navbar_content.classList.remove('faint-out')
-        navbar_content.classList.add('faint-in')
-        content.style.display = 'flex';
-    } else {
-        navbar_toggler.classList.remove('smooth-left-move');
-        navbar_toggler.classList.add('smooth-right-move');
-        flag = false
-        navbar_content.classList.remove('faint-in')
-        navbar_content.classList.add('faint-out')
-        content.style.display = 'none'
+/**
+ * hide navbar on resize
+ */
+window.addEventListener('resize', ()=> {
+    if (window.innerWidth >= 620){
+        navbar_links.classList.contains('active_navbar') ? 
+            navbar_links.classList.remove('active_navbar'): console.log()
     }
 })
